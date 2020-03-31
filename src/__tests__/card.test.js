e469bd7d764adca4aa79d1ddcda59e1a85e85992
Reuben Remy <reubenremy@gmail.com>
2020-03-30 22:27:10 -0400

    Added testing directory

---
+++ src/__tests__/card.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Card from '../components/card';
+
---
+++ src/components/card.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = ({ user }) => (
+    return <div />
+);
+
+export default Card;
